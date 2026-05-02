8486eaa0dd5ef644ad2f062b6ef679b2f31011c4
nilaiman <29336846+nilaiman@users.noreply.github.com>
2026-05-02 11:53:48 -0300

    update

--- src/new-site/components/ProcessBanner/ProcessBanner.tsx
+++ src/new-site/components/ProcessBanner/ProcessBanner.tsx
@@ -5,31 +5,28 @@ function ProcessBanner() {
   const [currentIndex, setCurrentIndex] = useState(0)
 
   const ads = [
-  {
-    id: 1,
-    desktop: 'public/banner-1.png',
-    mobile: 'public/planejamento-mobile.jpg',
-    alt: 'Planejamento'
-  },
-  {
-    id: 2,
-    desktop: 'public/banner-2.png',
-    mobile: 'public/desenvolvimento-mobile.jpg',
-    alt: 'Desenvolvimento'
-  },
-  {
-    id: 3,
-    desktop: 'public/banner-3.png',
-    mobile: 'public/integracoes-mobile.jpg',
-    alt: 'Integrações'
-  },
-  {
-    id: 4,
-    desktop: 'public/banner-4.png',
-    mobile: 'public/experiencia-app-mobile-4.png',
-    alt: 'Experiência tipo app'
-  }
-]
+    {
+      id: 1,
+      image: 'public/banner-1.png',
+      alt: 'Planejamento'
+    },
+    {
+      id: 2,
+      image: 'public/banner-2.png',
+      alt: 'Desenvolvimento'
+    },
+    {
+      id: 3,
+      image: 'public/banner-3.png',
+      alt: 'Integrações'
+    },
+    {
+      id: 4,
+      image: 'public/banner-4.png',
+      alt: 'Experiência tipo app'
+    }
+  ]
+
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % ads.length)
@@ -56,14 +53,9 @@ function ProcessBanner() {
 
           <div className="carousel-slide">
             <img 
-              src={ads[currentIndex].desktop} 
+              src={ads[currentIndex].image} 
               alt={ads[currentIndex].alt}
-              className="process-desktop"
-            />
-            <img 
-              src={ads[currentIndex].mobile} 
-              alt={ads[currentIndex].alt}
-              className="process-mobile"
+              className="process-image"
             />
           </div>
 
